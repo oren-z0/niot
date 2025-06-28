8df4441e0c3ebc4c29c630687f6c498360e3abce
Oren <orenz0@protonmail.com>
2025-06-28 22:47:39 +0300

    titles

--- components/Features.tsx
+++ components/Features.tsx
@@ -41,10 +41,11 @@ const Features = () => {
     <section className="container mx-auto px-4 py-12 bg-gray-50 dark:bg-gray-900 transition-colors duration-200 rounded-lg">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center">
-          <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-900 dark:text-white mb-4">Features</h2>
+          <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-900 dark:text-white mb-4">
+            Every Payment Becomes a <i>Zap</i>
+          </h2>
           <p className="mt-8 text-xl text-gray-600 dark:text-gray-300 font-light">
-            Highlight cool features of your app using the beautifully designed custom cards with icons. You can use any
-            icon you want.
+            Create LNURL-Pay links that create a <i>Zap</i> event when paid.
           </p>
         </div>
         <div className="mt-10">
--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <header className="bg-white dark:bg-black shadow-sm dark:border-b dark:border-gray-800">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
         <div className="flex items-center">
-          <span className="text-xl dark:text-gray-100">Next JS Starter Template</span>
+          <span className="text-xl dark:text-gray-100">niot.space</span>
         </div>
         <nav className="flex items-center">
           <ul className="flex space-x-2 mr-2">
@@ -20,10 +20,11 @@ export default function Header() {
             </li>
             <li>
               <Link
-                href="/contact"
+                href="https://njump.me/nprofile1qqsrx9hzmz8lj8ss38r4lmkumza2yfvtg4z45wc4dtmp04lv0x69legpz4mhxue69uhhyetvv9ujuerpd46hxtnfduhszrnhwden5te0dehhxtnvdakz7qgkwaehxw309ash2arg9ehx7um5wgcjucm0d5hsfa7mst"
+                target="_blank"
                 className="text-sm text-gray-800 dark:text-white px-4 py-2 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors"
               >
-                Contact
+                Follow Me
               </Link>
             </li>
           </ul>
