ac400a71687d0befe2b06aa980d0de283542a2d7
Oren <orenz0@protonmail.com>
2025-06-30 03:12:35 +0300

    fix footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,8 +6,8 @@ const Footer = () => {
   return (
     <footer className="bg-white border-t border-gray-100 dark:bg-black shadow-sm text-gray-400 py-6 dark:border-t dark:border-gray-800">
       <div className="container mx-auto px-4 flex flex-wrap justify-center sm:justify-between items-center text-sm">
-        <p className="ml-4">&copy; {currentYear} oren-z0. All rights reserved.</p>
-        <div className="flex space-x-4 mt-2 mr-4 sm:mt-0">
+        <p>&copy; {currentYear} oren-z0. All rights reserved.</p>
+        <div className="ml-4">
           <a href="https://github.com/oren-z0/niot" aria-label="GitHub" className="hover:text-gray-300">
             <AiOutlineGithub className="w-5 h-5" />
           </a>
