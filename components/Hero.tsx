6a76a23575ca79d357a0c2c4a9449d502867afcb
Oren <orenz0@protonmail.com>
2025-06-30 10:24:31 +0300

    replace italic

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ export default function Hero() {
   return (
     <section className="text-center my-24 mx-4 sm:mx-8 md:mx-16 lg:mx-24">
       <h1 className="font-serif text-3xl tracking-tight sm:text-5xl md:text-6xl lg:text-7xl dark:text-white mb-6">
-        nIoT.<i>space</i>
+        nIoT.<span className="italic">space</span>
       </h1>
       <p className="text-xl text-gray-600 dark:text-gray-300 mb-12 font-light">
         Useful tools for Nostr & IoT
--- components/PayToZap.tsx
+++ components/PayToZap.tsx
@@ -122,10 +122,10 @@ const PayToZap = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center">
           <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-900 dark:text-white mb-4">
-            Every Payment Becomes a <i>Zap</i>
+            Every Payment Becomes a <span className="italic">Zap</span>
           </h2>
           <p className="mt-8 text-xl text-gray-600 dark:text-gray-300 font-light">
-            Create LNURL-Pay links that create a <i>Zap</i> event when paid.
+            Create LNURL-Pay links that create a <span className="italic">Zap</span> event when paid.
           </p>
         </div>
         <div className="mt-10">
