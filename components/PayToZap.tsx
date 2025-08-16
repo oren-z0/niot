aa051956205f2c82cb13d31faccd14d25dd283ff
Oren <orenz0@protonmail.com>
2025-08-16 12:18:53 +0300

    AutoZapper

--- components/PayToZap.tsx
+++ components/PayToZap.tsx
@@ -143,7 +143,7 @@ const PayToZap = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center flex flex-col">
           <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-gray-900 dark:text-white mb-4">
-            Every Payment Becomes a <span className="italic">Zap</span>
+            AutoZapper
           </h2>
           <p className="mt-8 text-xl text-gray-600 dark:text-gray-300 font-light">
             Create LNURL-Pay links that create <span className="italic">Zap</span> events when paid.
