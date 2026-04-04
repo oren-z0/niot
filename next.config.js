bbfe073c5ff74e92e664b519f4759abb02ae44f5
Oren <orenz0@protonmail.com>
2026-04-05 01:08:08 +0300

    video redirect

--- next.config.js
+++ next.config.js
@@ -2,7 +2,17 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async redirects() {
+    // timelockrecovery-specter was released with a link to https://niot.space/a3JwIlQqwcb8WLEe.mp4
+    // instead of https://files.niot.space/a3JwIlQqwcb8WLEe.mp4
+    return [
+      {
+        source: "/a3JwIlQqwcb8WLEe.mp4",
+        destination: "https://files.niot.space/a3JwIlQqwcb8WLEe.mp4",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
